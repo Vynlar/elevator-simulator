2c95f6e2a54889e8ac95ea77df4cde30947b08fa
Adrian Aleixandre <lightningzygella@gmail.com>
2019-03-06 21:52:28 -0700

    added compiled assets

---
+++ docs/precache-manifest.d80512a70fef2759300e13351bf515e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac07313edcaa64cafb65",
+    "url": "/static/js/main.ac07313e.chunk.js"
+  },
+  {
+    "revision": "d7325659289be941a93a",
+    "url": "/static/js/1.d7325659.chunk.js"
+  },
+  {
+    "revision": "ac07313edcaa64cafb65",
+    "url": "/static/css/main.cca5f21e.chunk.css"
+  },
+  {
+    "revision": "d7325659289be941a93a",
+    "url": "/static/css/1.fb9d6c0f.chunk.css"
+  },
+  {
+    "revision": "44c9957da1344f8cd28086b603877ac0",
+    "url": "/index.html"
+  }
+];
